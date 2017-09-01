8a970e75b31c4870ac645ffd43d143e20217e48e
Anuj Sharma <anuj_sharma401@yahoo.com>
2017-09-01 21:40:25 +0530

    Update erlang_solutions.rb

--- cookbooks/yum-erlang_solutions/attributes/erlang_solutions.rb
+++ cookbooks/yum-erlang_solutions/attributes/erlang_solutions.rb
@@ -1,8 +1,8 @@
 case node['platform']
-  when  'amazon', 'centos'
-default['yum']['erlang_solutions']['baseurl'] = "http://packages.erlang-solutions.com/rpm/centos/7/$basearch"
+when  'amazon', 'centos'
+default['yum']['erlang_solutions']['baseurl'] = "https://packages.erlang-solutions.com/rpm/centos/$releasever/$basearch"
 default['yum']['erlang_solutions']['description'] = "Centos #{node['platform_version'].to_i} - $basearch - Erlang Solutions"
-default['yum']['erlang_solutions']['gpgkey'] = 'http://packages.erlang-solutions.com/debian/erlang_solutions.asc'
+default['yum']['erlang_solutions']['gpgkey'] = 'https://packages.erlang-solutions.com/rpm/erlang_solutions.asc'
 default['yum']['erlang_solutions']['gpgcheck'] = false
 default['yum']['erlang_solutions']['enabled'] = true
 default['yum']['erlang_solutions']['managed'] = true
